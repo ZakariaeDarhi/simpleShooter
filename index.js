0c6a15773f08abb8f0328dffa4babf72aad48f2d
zakariae darhi <zakariadarhi@gmail.com>
2023-07-10 19:14:46 +0100

    added bullets and enemies

--- index.js
+++ index.js
@@ -1,97 +1,159 @@
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
 
-const mousePosText = document.getElementById('mouse-pos');
-let mousePos = { x: undefined, y: undefined };
-
-window.addEventListener('mousemove', (e) => {
-    if (e.target == canvas) {
-        var rect = e.target.getBoundingClientRect();
-        mousePos = { x: e.clientX - rect.left, y: e.clientY - rect.top };
-        mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
-    }
-
-});
-
-document.addEventListener('keydown', keydown)
-document.addEventListener('keyup', keyup)
-
 canvas.width = 600;
 canvas.height = 400;
-const player = {
-    x: canvas.width / 2 - 30,
-    y: canvas.height / 2 - 30,
-    size: 30,
-    speed: 5,
-    dx: 0,
-    dy: 0
+
+class Player {
+    constructor(x, y, size, speed, dx, dy) {
+        this.x = x
+        this.y = y
+        this.size = size
+        this.speed = speed
+        this.dx = dx
+        this.dy = dy
+    }
+    drawPlayer() {
+        ctx.fillStyle = 'black'
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+    move() {
+        this.drawPlayer();
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.detectWalls()
+    }
+    detectWalls() {
+        // Left wall
+        if (this.x < 0) {
+            this.x = 0;
+        }
+
+        // Right Wall
+        if (this.x + this.size > canvas.width) {
+            this.x = canvas.width - this.size;
+        }
+
+        // Top wall
+        if (this.y < 0) {
+            this.y = 0;
+        }
+
+        // Bottom Wall
+        if (this.y + this.size > canvas.height) {
+            this.y = canvas.height - this.size;
+        }
+    }
+    dirDown() {
+        this.dy = this.speed
+    }
+    dirUp() {
+        this.dy = -this.speed
+    }
+    dirLeft() {
+        this.dx = -this.speed
+    }
+    dirRight() {
+        this.dx = this.speed
+    }
 }
 
-function drawLine() {
-    if (mousePos.x) {
+class Bullet {
+    constructor(x, y, radius, speed, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.speed = speed
+        this.velocity = velocity
+    }
+    drawBullet() {
         ctx.beginPath();
-        ctx.moveTo(player.x + player.size / 2, player.y + player.size / 2)
-        ctx.lineTo(mousePos.x, mousePos.y)
-        ctx.stroke()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = 'black'
+        ctx.fill()
     }
-}
-
-function drawPlayer() {
-    ctx.fillRect(player.x, player.y, player.size, player.size);
-}
-
-function move() {
-    player.x += player.dx;
-    player.y += player.dy;
-}
-
-function detectWalls() {
-    // Left wall
-    if (player.x < 0) {
-        player.x = 0;
+    move() {
+        this.drawBullet();
+        this.x += this.velocity.x * this.speed;
+        this.y += this.velocity.y * this.speed;
     }
 
-    // Right Wall
-    if (player.x + player.size > canvas.width) {
-        player.x = canvas.width - player.size;
+}
+
+class Enemy {
+    constructor(x, y, radius, speed, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.speed = speed
+        this.color = color
+        this.velocity = velocity
+    }
+    drawEnemy() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color;
+        ctx.fill()
+    }
+    move() {
+        this.drawEnemy();
+        this.x += this.velocity.x * this.speed;
+        this.y += this.velocity.y * this.speed;
+        this.updateDir()
+    }
+    updateDir() {
+        const angle = Math.atan2(player.y + player.size / 2 - this.y, player.x + player.size / 2 - this.x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        this.velocity = velocity;
     }
 
-    // Top wall
-    if (player.y < 0) {
-        player.y = 0;
-    }
-
-    // Bottom Wall
-    if (player.y + player.size > canvas.height) {
-        player.y = canvas.height - player.size;
-    }
 }
 
-function dirDown() {
-    player.dy = player.speed
-}
-function dirUp() {
-    player.dy = -player.speed
-}
-function dirLeft() {
-    player.dx = -player.speed
-}
-function dirRight() {
-    player.dx = player.speed
+const player = new Player(canvas.width / 2 - 30, canvas.height / 2 - 30, 30, 3, 0, 0)
+const bullets = [];
+const enemies = []
+let id;
+
+function spawnEnemies() {
+    id = setInterval(() => {
+        const random = Math.random() * 20
+        const radius = random > 10 ? random : 10;
+        const x = Math.random() < 0.5 ? 0 - radius : canvas.width
+        const y = Math.random() < 0.5 ? 0 - radius : canvas.height
+        const color = '#595959'
+
+        const angle = Math.atan2(player.y - player.size / 2 - y, player.x - player.size / 2 - x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        enemies.push(
+            new Enemy(x, y, radius, 1, color, velocity)
+        )
+        if (enemies.length > 10) {
+            clearInterval(id)
+        }
+    }, 1000)
 }
 
-function keydown(e) {
+document.addEventListener('keydown', (e) => {
     if (e.key === "ArrowLeft") {
-        dirLeft();
+        player.dirLeft();
     } else if (e.key === "ArrowRight") {
-        dirRight();
+        player.dirRight();
     } else if (e.key === "ArrowUp") {
-        dirUp();
+        player.dirUp();
     } else if (e.key === "ArrowDown") {
-        dirDown();
+        player.dirDown();
     }
-}
-function keyup(e) {
+})
+document.addEventListener('keyup', (e) => {
     if (
         e.key === "ArrowLeft" ||
         e.key === "ArrowRight" ||
@@ -101,20 +163,43 @@ function keyup(e) {
         player.dx = 0
         player.dy = 0
     }
-}
+})
 
+window.addEventListener('click', (e) => {
+    if (e.target == canvas) {
+        var rect = e.target.getBoundingClientRect();
+        const mousePos = { x: Math.floor(e.clientX - rect.left), y: Math.floor(e.clientY - rect.top) };
+        const angle = Math.atan2(mousePos.y - player.y - player.size / 2, mousePos.x - player.x - player.size / 2)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        bullets.push(
+            new Bullet(
+                player.x + player.size / 2,
+                player.y + player.size / 2,
+                5,
+                5,
+                velocity
+            )
+        )
+    }
 
+});
 
 
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPlayer()
-    drawLine()
-    move();
-    detectWalls()
+    player.move();
+    bullets.forEach((bullet) => {
+        bullet.move();
+    })
+    enemies.forEach((enemie) => {
+        enemie.move();
+    })
     requestAnimationFrame(update)
 }
 
-
+spawnEnemies()
 update()
